348187bc099b29e0a7f870f9d8c495d770a956d3
Ashley Bunch <bunchley007@gmail.com>
2025-04-10 11:52:35 -0600

    button changes to an x when clicked

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function myFunction(iconDisplay) {
+  let x = document.getElementById("myTopNav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+  console.log("icon display", iconDisplay);
+  iconDisplay.classList.toggle("change");
+}
